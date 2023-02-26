69d404b9c8be6605b305dad19ddf46cfa4cd2b46
Valentin Lehmann <info@valentinlehmann.de>
2023-02-26 17:36:51 +0100

    fix: removed CORS allowed methods POST and PUT

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
